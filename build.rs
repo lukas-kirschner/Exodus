0127408a38f9a16c394a09a5c4c96eb60f42a824
Lukas Kirschner <lukas@lukaskirschner.de>
2023-02-18 18:16:01 +0100

    Fixed Type Error in build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 fn main() {
     let mut git_hash: String = "<unknown hash>".to_string();
     if let Ok(output) = Command::new("git")
-        .args(&["rev-parse", "--short", "HEAD"])
+        .args(["rev-parse", "--short", "HEAD"])
         .output()
     {
         if let Ok(parsed_hash) = String::from_utf8(output.stdout) {
@@ -16,7 +16,7 @@ fn main() {
 
     let mut git_date: String = "<unknown date>".to_string();
     if let Ok(output) = Command::new("git")
-        .args(&["log", "-n", "1", "--format=%cs"])
+        .args(["log", "-n", "1", "--format=%cs"])
         .output()
     {
         if let Ok(parsed_date) = String::from_utf8(output.stdout) {
