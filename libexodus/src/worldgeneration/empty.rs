da8f4df8fb5f829095963f29f770dc136b79dac2
Lukas Kirschner <lukas@lukaskirschner.de>
2024-03-17 12:46:25 +0100

    Implemented Border and Empty Maps

---
+++ libexodus/src/worldgeneration/empty.rs
@@ -0,0 +1,50 @@
+use crate::world::GameWorld;
+use crate::worldgeneration::{WorldGenerationAlgorithm, WorldGenerationError};
+use std::num::TryFromIntError;
+
+#[derive(Clone)]
+pub(super) struct Empty {
+    pub width: u32,
+    pub height: u32,
+}
+
+impl WorldGenerationAlgorithm for Empty {
+    fn generate(&self) -> Result<GameWorld, WorldGenerationError> {
+        match (self.width, self.height) {
+            (0, _) => Err(WorldGenerationError::InvalidWidth { width: self.width }),
+            (_, 0) => Err(WorldGenerationError::InvalidHeight {
+                height: self.height,
+            }),
+            (w_u32, h_u32) => match usize::try_from(w_u32) {
+                Ok(w) => match usize::try_from(h_u32) {
+                    Ok(h) => Ok(GameWorld::new(w, h)),
+                    Err(e) => Err(WorldGenerationError::HeightOutOfRange { e }),
+                },
+                Err(e) => Err(WorldGenerationError::WidthOutOfRange { e }),
+            },
+        }
+    }
+
+    fn width(&self) -> &u32 {
+        &self.width
+    }
+
+    fn height(&self) -> &u32 {
+        &self.height
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::tiles::{TeleportId, Tile};
+    use crate::worldgeneration::tests::assert_map_content_matches;
+    use crate::worldgeneration::{build_generator, WorldGenerationKind};
+
+    #[test]
+    fn test_generate_2x2_map() {
+        let algo = build_generator(WorldGenerationKind::Empty, 2, 2);
+        let result = algo.generate();
+        assert!(result.is_ok());
+        let map = result.unwrap();
+        assert_map_content_matches("  \n  ", &map);
+    }
+}
--- libexodus/src/worldgeneration/mod.rs
+++ libexodus/src/worldgeneration/mod.rs
@@ -1,4 +1,11 @@
+mod border;
+mod empty;
+
 use crate::tiles::Tile;
+use crate::world::GameWorld;
+use crate::worldgeneration::border::Border;
+use crate::worldgeneration::empty::Empty;
+use std::num::TryFromIntError;
 use strum_macros::EnumIter;
 
 #[derive(Eq, PartialEq, Default, EnumIter, Clone)]
@@ -16,6 +23,7 @@ pub enum WorldGenerationKind {
         color: Tile,
     },
 }
+
 #[derive(Eq, PartialEq, Default, EnumIter, Copy, Clone)]
 pub enum WorldSize {
     #[default]
@@ -31,6 +39,7 @@ pub enum WorldSize {
         height: u32,
     },
 }
+
 const SIZE_5MX: (u32, u32) = (35, 15);
 const SIZE_SMALL: (u32, u32) = (22, 10);
 const SIZE_MEDIUM: (u32, u32) = (30, 14);
@@ -68,6 +77,105 @@ impl WorldGenerationKind {
             WorldGenerationKind::Border { .. } => false,
             WorldGenerationKind::Filled { .. } => false,
             WorldGenerationKind::Labyrinth { .. } => true,
+        }
+    }
+}
+
+/// An error that might occur during a map generation
+#[derive(Debug)]
+pub enum WorldGenerationError {
+    InvalidWidth { width: u32 },
+    InvalidHeight { height: u32 },
+    WidthOutOfRange { e: TryFromIntError },
+    HeightOutOfRange { e: TryFromIntError },
+    InvalidBorderWidth { border_width: u32 },
+}
+
+pub trait WorldGenerationAlgorithm {
+    /// Generate the world using this algorithm.
+    /// This may take a long time, depending on the algorithm.
+    fn generate(&self) -> Result<GameWorld, WorldGenerationError>;
+    /// Get the width of the generated map
+    fn width(&self) -> &u32;
+    /// Get the height of the generated map
+    fn height(&self) -> &u32;
+}
+
+/// Build the generator matching the given parameters
+pub fn build_generator(
+    parameters: WorldGenerationKind,
+    width: u32,
+    height: u32,
+) -> Box<dyn WorldGenerationAlgorithm> {
+    match parameters {
+        WorldGenerationKind::Empty => Box::new(Empty { width, height }),
+        WorldGenerationKind::Border {
+            width: border_width,
+            color,
+        } => Box::new(Border {
+            width,
+            height,
+            color,
+            border_width,
+        }),
+        WorldGenerationKind::Filled { .. } => Box::new(Empty { width, height }),
+        WorldGenerationKind::Labyrinth { .. } => Box::new(Empty { width, height }),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tiles::{TeleportId, Tile};
+    use crate::world::GameWorld;
+    use crate::worldgeneration::{build_generator, WorldGenerationKind};
+
+    /// Assert that the given map's content matches the given string.
+    /// The string must be given as 2D-Map, with "W" being a Wall tile, "N" being a
+    /// Nature Wall Tile and " " being Air.
+    pub fn assert_map_content_matches(expected: &str, map: &GameWorld) {
+        print_map(map);
+        let lines: Vec<&str> = expected.split("\n").filter(|l| l.len() > 0).collect();
+        let expected_h = lines.len();
+        let expected_w = lines[0].len();
+        assert_eq!(expected_h, map.height());
+        assert_eq!(expected_w, map.width());
+        for (y, line) in lines.iter().enumerate() {
+            assert_eq!(expected_w, line.len());
+            for (x, tile) in line.chars().enumerate() {
+                let my = (map.height() - y - 1) as i32;
+                assert_eq!(
+                    &match tile {
+                        'W' => Tile::WALL,
+                        'N' => Tile::WALLNATURE,
+                        ' ' => Tile::AIR,
+                        e => panic!("Unrecognized tile: {}", e),
+                    },
+                    map.get(x as i32, my).unwrap(),
+                    "Expected {} tile at position {},{} - got: {}",
+                    tile,
+                    x,
+                    my,
+                    map.get(x as i32, my).unwrap()
+                );
+            }
+        }
+    }
+    /// Debug-Print the given map's content to stdout
+    pub fn print_map(map: &GameWorld) {
+        for y in map.height() - 1..=0 {
+            for x in 0..map.width() {
+                print!(
+                    "{}",
+                    match map.get(x as i32, y as i32) {
+                        Some(Tile::WALL) => 'W',
+                        Some(Tile::WALLNATURE) => 'N',
+                        Some(Tile::AIR) => ' ',
+                        Some(t) => panic!("Unsupported tile found in map: {} at {},{}", t, x, y),
+                        None => panic!("Illegal Coordinate {},{}", x, y),
+                    }
+                );
+            }
+            print!("\n");
         }
     }
 }
